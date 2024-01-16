05b75191818b71cab460e99d0b61d5ae0bd94c99
Rafael Lemos <rhplemos@gmail.com>
2024-01-16 14:29:38 -0300

    feat: top app bar

--- app/src/main/java/com/example/baseproject/MainActivity.kt
+++ app/src/main/java/com/example/baseproject/MainActivity.kt
@@ -1,57 +1,61 @@
 package com.example.champ
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Surface
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Refresh
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
         setContent {
-            Surface(
-                modifier = Modifier.fillMaxSize(),
-//                    color = MaterialTheme.colorScheme.onPrimary
-            ) {
-                Greeting("oi")
+            MaterialTheme {
+                Column(modifier = Modifier.padding(10.dp)) {
+                    TopAppBar()
+                }
             }
         }
     }
+}
 
-    @Composable
-    fun Greeting(name: String, modifier: Modifier = Modifier) {
-
-        Column(
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center,
-            modifier = Modifier
-                .fillMaxSize()
-                .background(Color.LightGray)
-
-        ) {
-            Text(
-                text = "Water tracker",
-                modifier = modifier
-            )
-
-            Column(
-                horizontalAlignment = Alignment.CenterHorizontally,
-                verticalArrangement = Arrangement.Center,
-                modifier = Modifier.fillMaxSize()
-            ) {
-                Text(text = "foi?")
-            }
+@Composable
+fun TopAppBar() {
+    Row(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        IconButton(onClick = { /*TODO*/ }) {
+            Icon(imageVector = Icons.Default.Refresh, contentDescription = "Refresh")
         }
+        Text(text = "ChampScores", style = MaterialTheme.typography.headlineMedium)
+        IconButton(onClick = { /*TODO*/ }) {
+            Icon(imageVector = Icons.Default.Favorite, contentDescription = "Refresh")
+        }
+    }
+}
+
+@Preview
+@Composable
+fun TopAppBarPreview() {
+    MaterialTheme {
+        TopAppBar()
     }
 }
