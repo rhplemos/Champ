e8da65e5dba85a6cad76c7bbc6b107c593f80a3e
Rafael Lemos <rhplemos@gmail.com>
2024-02-15 20:37:10 -0300

    feat: my apiservice

---
+++ app/src/main/java/com/example/baseproject/data/remote/ApiService.kt
@@ -0,0 +1,11 @@
+package com.example.baseproject.data.remote
+
+import com.example.baseproject.data.remote.models.Match
+import com.example.baseproject.util.BASE_URL
+import retrofit2.http.GET
+
+interface ApiService {
+
+    @GET(BASE_URL)
+    suspend fun getMatches(): Match
+}
