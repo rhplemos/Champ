26f5b4b1ad5868ea6097b3a068d09ea94bb59445
Rafael Lemos <rhplemos@gmail.com>
2024-02-15 20:50:00 -0300

    feat: network module

---
+++ app/src/main/java/com/example/baseproject/di/NetworkModule.kt
@@ -0,0 +1,39 @@
+package com.example.baseproject.di
+
+import com.example.baseproject.data.remote.ApiService
+import com.example.baseproject.util.BASE_URL
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+
+    @Provides
+    fun okHttp(): OkHttpClient {
+        val loggingInterceptor = HttpLoggingInterceptor().apply { level = HttpLoggingInterceptor.Level.BODY }
+        return OkHttpClient.Builder()
+            .addInterceptor(loggingInterceptor)
+            .build()
+    }
+
+    @Provides
+    fun retrofit(okHttpClient: OkHttpClient): Retrofit {
+        return Retrofit.Builder()
+            .client(okHttpClient)
+            .addConverterFactory(GsonConverterFactory.create())
+            .baseUrl(BASE_URL)
+            .build()
+    }
+
+    fun apiService(retrofit: Retrofit): ApiService {
+        return retrofit.create(ApiService::class.java)
+    }
+
+}
